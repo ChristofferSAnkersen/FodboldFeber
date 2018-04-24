c9e4410bd9ea56b5d3f76017e269f76ff4e499d6
Simon Aagaard <simo136g@edu.eal.dk>
2018-04-24 14:42:00 +0200

    Start på sql connection i "DataAccess klassen"

--- FodboldFeber/Model/DataAccess.cs
+++ FodboldFeber/Model/DataAccess.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace FodboldFeber.Model
 {
-    class DataAccess
+    public class DataAccess
     {
+        private static string connectionString = "Server=EALSQL1.eal.local; Database=DB2017_A27; User Id= USER_A27; Password=SesamLukOp_27;";
+        public void Access()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd1 = new SqlCommand();
+                }
+                catch(SqlException e)
+                {
+                    Console.WriteLine("Det virker ikke :(");
+                }
+            }
+        }
     }
+
+
 }
