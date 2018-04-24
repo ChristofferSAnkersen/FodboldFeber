3d22f7c25b7160200f5f73e767b666b9ce96561a
Simon Aagaard <simo136g@edu.eal.dk>
2018-04-24 13:42:42 +0200

    Ændring af mainvindow til frontpage og fikset fejl med "kunne ikke finde ressource"

--- FodboldFeber/App.xaml.cs
+++ FodboldFeber/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using FodboldFeber;
 
 namespace FodboldFeber
 {
@@ -13,5 +14,6 @@ namespace FodboldFeber
     /// </summary>
     public partial class App : Application
     {
+        
     }
 }
