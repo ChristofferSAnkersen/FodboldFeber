da8d8a7a11717af5e7895302d3f65e75e40667cb
ChristofferSAnkersen <chri5122@edu.eal.dk>
2018-04-24 14:08:59 +0200

    Users Properties + classes

---
+++ FodboldFeber/Model/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FodboldFeber.Model
+{
+    class Users
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string ClubAddress { get; set; }
+        public string ClubName { get; set; }
+        public string ClubPosition { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyAddress { get; set; }
+        public int CVR { get; set; }
+        public int UserType { get; set; }
+    }  
+}
