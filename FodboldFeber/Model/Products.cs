f71508faae4a95da23ef76d009e8d1c61c0674e8
ChristofferSAnkersen <chri5122@edu.eal.dk>
2018-04-24 14:40:42 +0200

    Orders + Products properties

--- FodboldFeber/Model/Products.cs
+++ FodboldFeber/Model/Products.cs
@@ -8,5 +8,25 @@ namespace FodboldFeber.Model
 {
     class Products
     {
+        public void AddProduct()
+        {
+
+        }
+        public void UpdateProduct()
+        {
+
+        }
+        public void DeleteProduct()
+        {
+
+        }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryID { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public int AmountInStock { get; set; }
+        public int AmountInRoute { get; set; }
+        public int ShippingPrice { get; set; }
     }
 }
