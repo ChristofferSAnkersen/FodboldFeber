2a2779918b3c498cf1d1018e8221efbdadec3c98
Frederik Landtved Jensen <fred753a@edu.eal.dk>
2018-05-07 12:36:47 +0200

    Items i shop

--- FodboldFeber/View/Shop.xaml.cs
+++ FodboldFeber/View/Shop.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,35 @@ namespace FodboldFeber.View
         public Shop()
         {
             InitializeComponent();
+        }
+
+        string connectionString = "Server=EALSQL1.eal.local; Database=DB2017_A27; User Id= USER_A27; Password=SesamLukOp_27;";
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                string ImageQuery = "SELECT * from Products where ProductName='" + lbl_klaphat.Content + "' ";
+                SqlCommand listCommands = new SqlCommand(ImageQuery, con);
+                SqlDataReader reader = listCommands.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string ImagePicture = reader.GetString(9).ToString();
+                    ImageSourceConverter converter = new ImageSourceConverter();
+                    this.img_product1.Source = (ImageSource)converter.ConvertFromString(ImagePicture);
+                }
+            }
+
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex + "Fejl");
+            }
+
         }
     }
 }
