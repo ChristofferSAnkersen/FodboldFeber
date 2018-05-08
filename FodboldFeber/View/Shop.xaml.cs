2345aa5385590f5e528a97e86263666ebdcf1fe3
Frederik Landtved Jensen <fred753a@edu.eal.dk>
2018-05-08 13:34:06 +0200

    Grid er lækkert. Search kommer senere

--- FodboldFeber/View/Shop.xaml.cs
+++ FodboldFeber/View/Shop.xaml.cs
@@ -25,33 +25,5 @@ namespace FodboldFeber.View
             InitializeComponent();
         }
 
-        string connectionString = "Server=EALSQL1.eal.local; Database=DB2017_A27; User Id= USER_A27; Password=SesamLukOp_27;";
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            try
-            {
-                SqlConnection con = new SqlConnection(connectionString);
-                con.Open();
-                string ImageQuery = "SELECT * from Products where ProductName='" + lbl_klaphat.Content + "' ";
-                SqlCommand listCommands = new SqlCommand(ImageQuery, con);
-                SqlDataReader reader = listCommands.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    string ImagePicture = reader.GetString(9).ToString();
-                    ImageSourceConverter converter = new ImageSourceConverter();
-                    this.img_product1.Source = (ImageSource)converter.ConvertFromString(ImagePicture);
-                }
-            }
-
-            catch (SqlException ex)
-            {
-                Console.WriteLine(ex + "Fejl");
-            }
-
-        }
     }
 }
