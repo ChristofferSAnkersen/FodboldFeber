d77cf1510989e801ef1bdbbdfdc62a91f996ae51
Lasse Duus Skjønnemann <lass8293@edu.eal.dk>
2018-05-18 13:10:07 +0200

    BtnSendEmailClick kan nu sende en email til den email der er skrevet in i txtEmail teksboksen.

--- FodboldFeber/View/PasswordRecovery.xaml.cs
+++ FodboldFeber/View/PasswordRecovery.xaml.cs
@@ -12,14 +12,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail; 
+using System.Web;
+using FodboldFeber.ViewModel;
 
 namespace FodboldFeber.View
 {
-    /// <summary>
-    /// Interaction logic for PasswordRecovery.xaml
-    /// </summary>
     public partial class PasswordRecovery : Page
     {
+        
+        SmtpClient client;
+        MailMessage msg;
+        CustomerVM cVM = new CustomerVM();
         public PasswordRecovery()
         {
             InitializeComponent();
@@ -27,7 +31,22 @@ namespace FodboldFeber.View
 
         private void BtnSendEmailClick(object sender, RoutedEventArgs e)
         {
-
+            msg = new MailMessage
+            {
+                From = new MailAddress("fodboldfeberprojekt@gmail.com")
+            };
+            msg.To.Add(new MailAddress(txtEmail.Text));
+            msg.Subject = "Kodeords Nulstilling";
+            msg.Body = "Dit kodeord er blevet nulstillet";
+            client = new SmtpClient("smtp.gmail.com");
+            client.Port = 587;
+            client.Credentials = new System.Net.NetworkCredential("fodboldfeberprojekt@gmail.com", "Fodboldfeber123");
+            client.EnableSsl = true;
+            client.Send(msg);
+            MessageBox.Show("Email Sendt!");
         }
+        
+
+
     }
 }
