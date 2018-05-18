8ebf997400064ba33cf199ddb879cc66f8743334
Simon Aagaard <simo136g@edu.eal.dk>
2018-05-18 13:09:43 +0200

    Tilføjelse af tests

--- FodboldFeber/Model/DataAccess.cs
+++ FodboldFeber/Model/DataAccess.cs
@@ -13,10 +13,10 @@ namespace FodboldFeber.Model
     {
         Login login = new Login();
         Authenticated auth = new Authenticated();
-        private static string connectionString = "Server=EALSQL1.eal.local; Database=DB2017_A27; User Id= USER_A27; Password=SesamLukOp_27;";
-        public void Access()
+        public static string connectionString = "Server=EALSQL1.eal.local; Database=DB2017_A27; User Id= USER_A27; Password=SesamLukOp_27;";
+        public string Access()
         {
-           
+            return connectionString;
         }
         public void InitializeLogin()
         {
