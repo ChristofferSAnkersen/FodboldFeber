69028a8ece0d22b4c33fb8fb55bf19562cd90ba7
Lasse Duus Skjønnemann <lass8293@edu.eal.dk>
2018-05-22 13:29:45 +0200

    Scrollemann med scrolleviewers. Har sgu snart flere scollviewers end mikkels har twitch viewes

--- FodboldFeber/View/PasswordRecovery.xaml.cs
+++ FodboldFeber/View/PasswordRecovery.xaml.cs
@@ -28,38 +28,38 @@ namespace FodboldFeber.View
         }
         private void BtnSendEmailClick(object sender, RoutedEventArgs e)
         {
-            if (cVM.CheckIfExist)
+            //   if (cVM.CheckIfExist)
+            //  {
+            SmtpClient client;
+            MailMessage msg;
+            // Generates random password since we cant send a reset link because we are doing a wpf app.
+            char[] newPassword = "qwertyuiopasdfghjklzxcvbnm1234567890".ToCharArray();
+            Random r = new Random();
+            String randomString = "";
+            for (int i = 0; i < 10; i++)
             {
-                SmtpClient client;
-                MailMessage msg;
-                // Generates random password since we cant send a reset link because we are doing a wpf app.
-                char[] newPassword = "qwertyuiopasdfghjklzxcvbnm1234567890".ToCharArray();
-                Random r = new Random();
-                String randomString = "";
-                for (int i = 0; i < 10; i++)
-                {
-                    randomString += newPassword[r.Next(0, 35)].ToString();
-                }
-                cVM.Password = randomString;
-                // Email message information
-                msg = new MailMessage();
-                msg.From = new MailAddress("fodboldfeberprojekt@gmail.com");
-                msg.To.Add(new MailAddress(txtEmail.Text));
-                msg.Subject = "Kodeords Nulstilling";
-                msg.Body = "Dit kodeord er midlertigdigt lavet op til " + randomString;
-                // Smtp information 
-                client = new SmtpClient("smtp.gmail.com");
-                client.Port = 587;
-                client.Credentials = new System.Net.NetworkCredential("fodboldfeberprojekt@gmail.com", "Fodboldfeber123");
-                client.EnableSsl = true;
-                client.Send(msg);
-                // MessageBox Confirmation 
-                MessageBox.Show("Email Sendt til dig!");
+                randomString += newPassword[r.Next(0, 35)].ToString();
             }
-            else
-            {
-                MessageBox.Show("Brugernavn og Email stemmer ikke overens");
-            }
+            cVM.Password = randomString;
+            // Email message information
+            msg = new MailMessage();
+            msg.From = new MailAddress("fodboldfeberprojekt@gmail.com");
+            msg.To.Add(new MailAddress(txtEmail.Text));
+            msg.Subject = "Kodeords Nulstilling";
+            msg.Body = "Dit kodeord er midlertigdigt lavet op til " + randomString;
+            // Smtp information 
+            client = new SmtpClient("smtp.gmail.com");
+            client.Port = 587;
+            client.Credentials = new System.Net.NetworkCredential("fodboldfeberprojekt@gmail.com", "Fodboldfeber123");
+            client.EnableSsl = true;
+            client.Send(msg);
+            // MessageBox Confirmation 
+            MessageBox.Show("Email Sendt til dig!"); 
+       //     }
+            //else
+            //{
+            //    MessageBox.Show("Brugernavn og Email stemmer ikke overens");
+            //}
         }
 
         private void BtnForgotUserNameClick(object sender, RoutedEventArgs e)
